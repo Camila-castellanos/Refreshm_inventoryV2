eec89fff95f327b3161ca00b8c2b80cfa4cf7764
Camila Castellanos <camilacastellanosbotero596@gmail.com>
2025-03-06 14:26:30 -0400

    feat: adding logic for create a list

--- resources/js/Lib/types.ts
+++ resources/js/Lib/types.ts
@@ -80,3 +80,13 @@ export interface Contact {
   created_at: string;
   updated_at: string;
 }
+
+export interface MailingList {
+  id: number;
+  title: string;
+  names: string;
+  emails: string;
+  user_id: number;
+  created_at: string;
+  updated_at: string;
+}
--- resources/js/Pages/Customers/MailList/Index.vue
+++ resources/js/Pages/Customers/MailList/Index.vue
@@ -6,7 +6,7 @@
         <DataTable
           title="Mailing List"
           @update:selected="handleSelection"
-          :items="[]"
+          :items="tableData"
           :headers="mailingListHeaders"
           :actions="mailingListActions"></DataTable>
       </ContactTabs>
@@ -21,15 +21,16 @@ import { useDialog } from "primevue";
 import { onMounted, Ref, ref } from "vue";
 import { mailingListHeaders } from "../IndexData";
 import MailList from "./MailList.vue";
-import { Contact } from "@/Lib/types";
+import { Contact, MailingList } from "@/Lib/types";
 
 const props = defineProps({
   contacts: Array<Contact>,
   email_templates: Array,
-  mailing_lists: Array,
+  mailing_lists: Array<MailingList>,
 });
 
 const dialog = useDialog();
+const tableData: Ref<MailingList[]> = ref([]);
 
 const mailingListActions = [
   {
@@ -61,6 +62,36 @@ function openAddMailList() {
 
 onMounted(() => {
   console.log(props);
+  tableData.value =
+    props?.mailing_lists?.map((list) => {
+      const emails: string[] = JSON.parse(list.emails);
+      const names: string[] = JSON.parse(list.names);
+      return {
+        ...list,
+        names: names.join(", "),
+        emails: emails.join(", "),
+        actions: [
+          {
+            label: "",
+            icon: "pi pi-pencil",
+            outlined: true,
+            action: () => console.log(list),
+          },
+          {
+            label: "",
+            icon: "pi pi-trash",
+            severity: "danger",
+            action: () => console.log(list),
+          },
+          {
+            label: "",
+            icon: "pi pi-envelope",
+            severity: "info",
+            action: () => console.log(list)
+          }
+        ],
+      };
+    }) ?? [];
 });
 
 defineOptions({ layout: AppLayout });
