7cb8accb33ac1318fa7d2a41dd19d496080048e0
Maria Camila <camilacastellanosbotero596@gmail.com>
2025-04-16 15:24:11 -0500

    little comment update

--- resources/views/sale-receipt.blade.php
+++ resources/views/sale-receipt.blade.php
@@ -16,8 +16,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <style>
         @page {
-        size: A4 portrait; /* Combinación de tamaño y orientación */
-        margin: 10mm; /* Márgenes muy pequeños para recibos */
+        size: A4 portrait; /* pdf size and orientation */
+        margin: 10mm; /* margin */
     }
     </style>
 </head>
