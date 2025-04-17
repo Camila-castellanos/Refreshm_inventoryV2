2b3e2bc84a816d1963fcc21f384a0f32aa580ae5
Maria Camila <camilacastellanosbotero596@gmail.com>
2025-04-17 17:48:38 -0500

    vendor fix on add items

--- resources/js/Pages/Accounting/AddItems.vue
+++ resources/js/Pages/Accounting/AddItems.vue
@@ -47,13 +47,13 @@ function parseItemsData() {
         return {
           ...item,
           location: `${name} - ${position}/${limit}`,
-          vendor: item.vendor.vendor == 'Unknown' ? "N/A" : item.vendor.vendor,
+          vendor: item.vendor?.vendor == 'Unknown' ? "N/A" : item.vendor?.vendor,
         };
       }
       return {
         ...item,
         location: "No storage information",
-        vendor: item.vendor.vendor == 'Unknown' ? "N/A" : item.vendor.vendor,
+        vendor: item.vendor?.vendor == 'Unknown' ? "N/A" : item.vendor?.vendor,
       };
     });
   console.log("data despues de filtrar: ", tableData.value);
