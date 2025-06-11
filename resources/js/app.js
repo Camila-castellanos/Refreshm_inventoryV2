9318cd608ad1c23fcb959c136460ec0c13c1b0b2
Maria Camila <camilacastellanosbotero596@gmail.com>
2025-06-11 16:50:52 -0500

    new global button style

--- resources/js/app.js
+++ resources/js/app.js
@@ -36,58 +36,77 @@ axios.interceptors.response.use(
 );
 
 const Noir = definePreset(Aura, {
-  semantic: {
-    primary: {
-      50: "{zinc.50}",
-      100: "{zinc.100}",
-      200: "{zinc.200}",
-      300: "{zinc.300}",
-      400: "{zinc.400}",
-      500: "{zinc.500}",
-      600: "{zinc.600}",
-      700: "{zinc.700}",
-      800: "{zinc.800}",
-      900: "{zinc.900}",
-      950: "{zinc.950}",
-    },
-    colorScheme: {
-      light: {
+    semantic: {
         primary: {
-          color: "{zinc.950}",
-          inverseColor: "#ffffff",
-          hoverColor: "{zinc.900}",
-          activeColor: "{zinc.800}",
+            50: '{zinc.50}',
+            100: '{zinc.100}',
+            200: '{zinc.200}',
+            300: '{zinc.300}',
+            400: '{zinc.400}',
+            500: '{zinc.500}',
+            600: '{zinc.600}',
+            700: '{zinc.700}',
+            800: '{zinc.800}',
+            900: '{zinc.900}',
+            950: '{zinc.950}'
         },
-        highlight: {
-          background: "{zinc.950}",
-          focusBackground: "{zinc.700}",
-          color: "#ffffff",
-          focusColor: "#ffffff",
-        },
-      },
-      dark: {
-        primary: {
-          color: "{zinc.50}",
-          inverseColor: "{zinc.950}",
-          hoverColor: "{zinc.100}",
-          activeColor: "{zinc.200}",
-        },
-        highlight: {
-          background: "rgba(250, 250, 250, .16)",
-          focusBackground: "rgba(250, 250, 250, .24)",
-          color: "rgba(255,255,255,.87)",
-          focusColor: "rgba(255,255,255,.87)",
-        },
-      },
+        
+        colorScheme: {
+            light: {
+                primary: {
+                    color: '{zinc.950}',
+                    inverseColor: '{zinc.50}',
+                    hoverColor: '{zinc.900}',
+                    activeColor: '{zinc.800}'
+                },
+                highlight: {
+                    background: '{zinc.950}',
+                    focusBackground: '{zinc.700}',
+                    color: '#ffffff',
+                    focusColor: '#ffffff'
+                }
+            },
+            dark: {
+                primary: {
+                    color: '{zinc.50}',
+                    inverseColor: '{zinc.950}',
+                    hoverColor: '{zinc.100}',
+                    activeColor: '{zinc.200}'
+                },
+                highlight: {
+                    background: 'rgba(250, 250, 250, .16)',
+                    focusBackground: 'rgba(250, 250, 250, .24)',
+                    color: 'rgba(255,255,255,.87)',
+                    focusColor: 'rgba(255,255,255,.87)'
+                }
+            }
+        }
     },
-  },
-  components: {
-    button: {
-      defineProps: {
-        severity: 'secondary'
-      }
+    components: {
+        button: {
+            colorScheme: {
+                light: {
+                    primary: {
+                    background: '#f1f5f9',
+                    color: '#475569',
+                    borderColor: 'transparent',
+                    hoverBackground: '#e2e8f0',
+                    hoverColor: '#475569',
+                    hoverBorderColor: 'transparent',
+              },
+            },
+                dark: {
+                    root: {
+                        background: '{surface.900}',
+                        color: '{surface.0}'
+                    },
+                    subtitle: {
+                        color: '{surface.400}'
+                    }
+                }
+            }
+        }
     }
-  }
 });
 
 createInertiaApp({
