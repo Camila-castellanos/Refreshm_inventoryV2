8aa515a06474d79dfa7ed11f1d5e3d49b97ef88a
Maria Camila <camilacastellanosbotero596@gmail.com>
2026-02-16 17:38:24 -0500

    email name fix

--- resources/js/Pages/Ecommerce/Orders/Index.vue
+++ resources/js/Pages/Ecommerce/Orders/Index.vue
@@ -1,8 +1,8 @@
 <template>
-    <AppLayout title="Ecommerce Orders">
+    <AppLayout :title="`Ecommerce Orders - ${market?.name || 'Market'}`">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Ecommerce Orders
+                Ecommerce Orders - {{ market?.name }}
             </h2>
         </template>
 
@@ -21,7 +21,20 @@
                             </template>
                         </Column>
                         <Column field="status" header="Status" sortable></Column>
-                        <Column field="payment_intent" header="Payment ID"></Column>
+                        <Column field="payment_intent" header="Payment">
+                            <template #body="slotProps">
+                                <a 
+                                    v-if="slotProps.data.payment_intent"
+                                    :href="getStripeUrl(slotProps.data.payment_intent)"
+                                    target="_blank"
+                                    class="inline-flex items-center gap-1 px-2 py-1 text-xs font-medium text-blue-600 bg-blue-50 hover:bg-blue-100 rounded transition-colors"
+                                >
+                                    <i class="pi pi-external-link"></i>
+                                    View in Stripe
+                                </a>
+                                <span v-else class="text-gray-400">-</span>
+                            </template>
+                        </Column>
                     </DataTable>
                 </div>
             </div>
@@ -36,6 +49,7 @@ import Column from 'primevue/column';
 
 defineProps({
     orders: Array,
+    market: Object,
     filters: Object,
 });
 
@@ -44,5 +58,9 @@ const formatCurrency = (value) => {
         style: 'currency',
         currency: 'USD',
     }).format(value);
+};
+
+const getStripeUrl = (paymentIntentId) => {
+    return `https://dashboard.stripe.com/payments/${paymentIntentId}`;
 };
 </script>
